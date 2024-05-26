b22b6d08839602ca828a3f7059d25ad4fd83cb4e
Eshaan Joshi <eshaanj@gmail.com>
2024-05-25 22:16:47 -0700

    modified and built main cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
 
 int main() {
+	vector<string> msg = {"Hello", "world!", "from", "main.cpp", "in", "VsCode", "C++"};
 	cout << "hello world!\n";
+
+	for (const string& s : msg) {
+		cout << s << "\n";
+	}
+
+	cout << endl;
+
 	return 0;
 }
 
